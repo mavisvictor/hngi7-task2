07ed9f71adc29468d2407598d01a8ed1574231a7
mavisvictor <66245780+mavisvictor@users.noreply.github.com>
2020-06-05 16:07:06 +0100

    new work, biko work o

--- scripts/Mavis_script.php
+++ scripts/Mavis_script.php
@@ -4,6 +4,7 @@
   $name = "Chijindu Mavis";
   $hng_id = "HNGi7 ID-03664";
   $language = "php";
+
 echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task email $email";
 
 ?>
