0043322af165fed5a8151c064a8ecf795abe0751
mavisvictor <66245780+mavisvictor@users.noreply.github.com>
2020-06-06 07:06:18 +0100

    Updated everything will the new script
    Please o

--- scripts/Mavischijindu_script.php
+++ scripts/Mavischijindu_script.php
@@ -1,8 +1,8 @@
 <?php 
 
  $email = "mavisvictor@gmail.com";
-  $name = "Chijindu Mavis";
-  $hng_id = "HNGi7 ID-03664";
+  $name = "Okonkwo Mavis";
+  $hng_id = "HNG-03664";
   $language = "php";
 echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task email $email";
 
